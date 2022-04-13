a35e2dec9265f8e045c4499d068de4b1d99b39c9
caseyfrost <caseyfrost13@gmail.com>
2022-04-12 19:56:26 -0700

    Added some comments to autoinc_spark

--- autoinc_spark.py
+++ autoinc_spark.py
@@ -1,10 +1,11 @@
 from pyspark import SparkContext
 from operator import add
 
-sc = SparkContext("local", "CarSales")
+# create raw rdd
+sc = SparkContext("local", "CarIncidents")
 raw_rdd = sc.textFile("data.csv")
 
-
+# create KV of vin, (type, make, year)
 def extract_vin_key_value(row):
     return row[2], (row[1], row[3], row[5])
 
@@ -13,13 +14,14 @@ def extract_vin_key_value(row):
 #     if x[0] and x[1]:
 #         return x
 
-
+# extract the make-year
 def extract_make_key_value(x):
     return f'{x[1]}-{x[2]}'
 
 
 vin_kv = raw_rdd.map(lambda x: extract_vin_key_value(x))
 
+# filter for the I type incidents
 enhance_make = vin_kv.groupByKey().flatMap(lambda kv: kv[1]).filter(lambda x: x[0] == 'I')
 
 make_kv = enhance_make.map(lambda x: extract_make_key_value(x))
